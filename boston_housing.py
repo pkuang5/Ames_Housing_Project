ffcb1cf30a54cfd3e0b0c2d94dd09b674d79f0d0
Patrick Kuang <patrickkuang@ResNet-62-63.resnet.ucsb.edu>
2019-01-27 17:15:59 -0800

    added expensive attribute

--- boston_housing.py
+++ boston_housing.py
@@ -3,4 +3,9 @@ train = pd.read_csv("all/train.csv")
 print(train.head())
 test = pd.read_csv("all/test.csv")
 print(test.head())
-#Example
+
+train["Expensive"] = float('NaN')
+train["Expensive"][train["SalePrice"] < train["SalePrice"].median()] = 0
+train["Expensive"][train["SalePrice"] >= train["SalePrice"].median()] = 1
+#print(train["Expensive"].value_counts(normalize = True))
+print(train["Expensive"][train["MSSubClass"] == 20].value_counts(normalize = True))
