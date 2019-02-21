de589a60ca7a6f30cc8ed00ac14e446efb718a52
Patrick Kuang <patrickkuang@169-231-122-182.wireless.ucsb.edu>
2019-02-21 11:09:31 -0800

    2/21

--- boston_housing.py
+++ boston_housing.py
@@ -5,19 +5,20 @@ import matplotlib.pyplot as plt
 from sklearn import tree
 from scipy import stats
 
-
-
+#reades and imports train and test dataframe 
 train = pd.read_csv("all/train.csv")
-#print(train.head())
 test = pd.read_csv("all/test.csv")
-#print(test.head())
 
+#makes copy of  dataframes
 train_copy = train
 test_copy = test
 
-
+#makes an array of the name of all attributes
 names = list(train)
-#plt.matshow(train.corr())
+
+names_test = list(train)
+names_test.pop(80)
+
 #locates deletes unimportant attributes
 deleted_attribute = [0,2,5,6,7,9,10,11,14,15,21,22,23,24,27,28,31,32,33,35,36,
                      37,39,40,42,45,47,48,50,51,52,53,55,57,58,60,63,64,66,67,
@@ -28,26 +29,31 @@ for j in reversed(deleted_attribute): #deletes unimportant attributes
 #array of attributues that need lienar regression
 linear_attribute = [1,2,9,10,12,15,16,18,19,20,24,26] 
 
-#converting object columns to int columns
-train_copy["MSZoning"] = train_copy.MSZoning.astype(float)
 
-#filling in empty values with median values for linear attributes
-for i in names:
-    for k in linear_attribute:
-        if i==names[k]:
-            train_copy[i] = train_copy[i].fillna(train_copy[i].median())
-            test_copy[i] = test_copy[i].fillna(test_copy[i].median())
+#filling in empty values with median values for all numerical attributes  in train and test dataframes 
+for i in names_test:
+    if train_copy[i].dtypes=='int64' or train_copy[i].dtypes=='float64':
+        train_copy[i] = train_copy[i].fillna(train_copy[i].median())
+    if test[i].dtypes=='int64' or test[i].dtypes=='float64':
+        test_copy[i] = test_copy[i].fillna(test_copy[i].median())
+            
+#converting object attrbutes to int type
+for i in names_test:
+    if train_copy[i].dtypes=="O":
+        train_copy[i] = train_copy[i].astype('category')
+        train_copy[i] = train_copy[i].cat.codes
+for i in names_test:
+    if test[i].dtypes=="O":
+        test[i] = test[i].astype('category')
+        test[i] = test[i].cat.codes
         
-
-train_copy2 = train_copy
-
 #for i in names:
 #    for k in linear_attribute:
 #        if i==names[k]:
 #            z_array = np.abs(stats.zscore(train_copy[i])) #creates an array z that holds the z-score (SD from mean
 #            z_outlier_indexes = np.where(z_array > 3) #creates an nested array of the indexes of the outliers (z-score>3)
 #            z_outlier_indexes = z_outlier_indexes[0] #converts nested array into an array
-#            train_copy2 = train_copy.drop(z_outlier_indexes) #creates train_copy2 which omits the outliers
+#            train_copy = train_copy.drop(z_outlier_indexes) #creates train_copy which omits the outliers
 
 
 #for i in names:
@@ -57,35 +63,35 @@ train_copy2 = train_copy
 #            print_linear = True;
 #    if print_linear==True:        
 #        plt.figure()
-#        ax = sns.regplot(x=train_copy2[i], y=train_copy2["SalePrice"]) 
+#        ax = sns.regplot(x=train_copy[i], y=train_copy["SalePrice"]) 
 #    else:
 #        plt.figure()
-#        ax = sns.stripplot(x=train_copy2[i], y=train_copy2["SalePrice"]) 
+#        ax = sns.stripplot(x=train_copy[i], y=train_copy["SalePrice"]) 
         
 #modeling via decision tree
 
       
 #creating target set (Sale Price)   
-target = train_copy2["SalePrice"].values
+target = train_copy["SalePrice"].values
 
 #creating features  set (all attributes except Sale Price)
-features = train[["LotFrontage","LotArea","TotalBsmtSF","MSSubClass"]].values
+features = train_copy[names].values
         
 # Fit your first decision tree: my_tree
 my_tree = tree.DecisionTreeClassifier()
 my_tree = my_tree.fit(features, target)
 
 # Look at the importance and score of the included features
-#print(my_tree.feature_importances_)
-#print(my_tree.score(features, target))
+print(my_tree.feature_importances_)
+print(my_tree.score(features, target))
 
-# Extract the features from the test set: Pclass, Sex, Age, and Fare.
-test_features = test[["LotFrontage","LotArea","TotalBsmtSF","MSSubClass"]].values
+# Extract the features from the test set
+test_features = test[names].values
 
 # Make your prediction using the test set
 my_prediction = my_tree.predict(test_features)
 
-# Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
+# Create a data frame with two columns: Id & SalePrice. SalePrice contains your predictions
 Id =np.array(test["Id"]).astype(int)
 my_solution = pd.DataFrame(my_prediction, Id, columns = ["SalePrice"])
 print(my_solution)
